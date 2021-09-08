99284af69b7c879e007807496e9e81013da6801f
Avnish <89906297+KingNoobX@users.noreply.github.com>
2021-09-08 06:52:13 +0530

    Add files via upload

---
+++ Cuple_music_bot/function/__init__.py
@@ -0,0 +1,5 @@
+from Cuple_music_bot.function.admins import admins
+from Cuple_music_bot.function.admins import get
+from Cuple_music_bot.function.admins import set
+
+__all__ = ["set", "get", "admins"]
