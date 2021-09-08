194a3cdfd767198d9234f43aa1383fb51d00cfe9
Avnish <89906297+KingNoobX@users.noreply.github.com>
2021-09-08 07:03:02 +0530

    Add files via upload

---
+++ Cuple_music_bot/services/queues/__init__.py
@@ -0,0 +1,7 @@
+from Cuple_music_bot.services.queues.queues import clear 
+from Cuple_music_bot.services.queues.queues import get
+from Cuple_music_bot.services.queues.queues import is_empty
+from Cuple_music_bot.services.queues.queues import put
+from Cuple_music_bot.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
